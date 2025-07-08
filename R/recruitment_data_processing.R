a63533422648918a6ff3ac9a34c7717e3a40068e
fja062 <fjaroszynska@gmail.com>
2025-07-08 09:28:32 +0200

    tidy data processing script

--- R/recruitment_data_processing.R
+++ R/recruitment_data_processing.R
@@ -1,28 +1,5 @@
 # seedling data cleaning #
 
-#load packages
-library("lubridate")
-library("googlesheets4")
-library("tidyverse")
-
-#source climate and plotting files
-#source("~/Documents/research/FunCaB/figures/plotting_dim.R")
-#source("~/Documents/research/FunCaB/climate/LT_climate.R")
-
-# load SPEI data
-load("~/OneDrive - University of Bergen/Research/FunCaB/Data/secondary/spei_20092019.RData")
-
-#load seedling and biomass data
-load("~/OneDrive - University of Bergen/Research/FunCaB/Data/secondary/composition_211123.RData")
-load("~/OneDrive - University of Bergen/Research/FunCaB/Data/secondary/communityBiomass_cleaned.RData")
-
-gs4_deauth() # works for googlesheets where link sharing is ON
-dat <- read_sheet(ss = "https://docs.google.com/spreadsheets/d/1eDVV6pAoeVfwaUGzdQuWrmN2_9s7q3EhbYQwbtvnP_M/edit#gid=595311948", sheet = "FUNCAB_recruitment2019", col_types = "c")
-
-# load dictionaries
-source("~/Documents/research/FunCaB/dictionaries.R")
-
-
 # biomass data from 2015/2016
 biomass <- community_1516 %>%
   select(siteID, blockID, turfID, biomassGram, biomassForb, biomassBryo, Treatment) %>% 
