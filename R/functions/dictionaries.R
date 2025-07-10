43719b81d68252e7c6b33b5fbddde1cd9572858c
fja062 <fjaroszynska@gmail.com>
2025-07-10 16:25:03 +0200

    add ttc-turf dictionary

--- R/functions/dictionaries.R
+++ R/functions/dictionaries.R
@@ -14,3 +14,39 @@ Alr ALR alr Alrust Alrust
 Fau FAU fau Fauske Fauske
 Ram RAM ram Rambera Rambera")
 }
+
+dict_TTC_turf <- function(){dict_TTC_turf <- read_delim(delim = ";", file =
+                              "TTtreat;plotID
+51 TTC;Fau1C
+57 TTC;Fau2C
+68 TTC;Fau4C
+73 TTC;Fau5C
+29 TTC;Alr1C
+31 TTC;Alr2C
+37 TTC;Alr3C
+134 TTC;Vik2C
+140 TTC;Vik3C
+141 TTC;Vik4C
+146 TTC;Vik5C
+101 TTC;Hog1C
+110 TTC;Hog2C
+115 TTC;Hog3C
+286 TTC;Ovs1C
+291 TTC;Ovs2C
+297 TTC;Ovs3C
+211 TTC;Arh1C
+222 TTC;Arh3C
+226 TTC;Arh4C
+263 TTC;Ves1C
+281 TTC;Ves4C
+194 TTC;Ram4C
+198 TTC;Ram5C
+6 TTC;Ulv2C
+11 TTC;Ulv3C
+236 TTC;Skj1C
+243 TTC;Skj2C
+246 TTC;Skj3C
+251 TTC;Skj4C
+511 TTC;Gud12C
+") 
+}
