2957bd6049840c9c964e642e3290c4efefafc381
fja062 <fjaroszynska@gmail.com>
2025-09-05 10:53:49 +0200

    adding bayes packages to _targets

--- _targets.R
+++ _targets.R
@@ -6,7 +6,7 @@ library(tarchetypes)
 
 # Set target options:
 tar_option_set(
-  packages = c("tidyverse", "dataDownloader", "vegan", "ggvegan", "Hmisc", "glue", "traitstrap", "dataDocumentation", "lme4", "broom", "broom.mixed", "lmerTest", "tidymodels", "sjPlot", "patchwork"), # packages that your targets need to run
+  packages = c("tidyverse", "dataDownloader", "vegan", "ggvegan", "Hmisc", "glue", "traitstrap", "dataDocumentation", "lme4", "broom", "broom.mixed", "lmerTest", "tidymodels", "sjPlot", "patchwork", "rstanarm", "bayesplot"), # packages that your targets need to run
   #format = "rds" # default storage format
 )
 
